196e4c846778b9fc4ddc62c01a8ebc0ce33e1677
charlene-johnson <charlene.j8234@gmail.com>
2020-07-10 13:48:10 -0400

    Changed frd FriendList componenet into a function component

--- friends/src/components/FriendList.js
+++ friends/src/components/FriendList.js
@@ -1,38 +1,79 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import { axiosWithAuth } from "../utils/axiosWithAuth";
 import Loader from 'react-loader-spinner';
 
-class FriendList extends React.Component {
-  state = {
-    friends: [],
-  };
+// class FriendList extends React.Component {
+//   state = {
+//     friends: [],
+//   };
 
-  componentDidMount() {
-    this.getData();
-  }
-  getData = () => {
-    axiosWithAuth()
-      .get("/api/friends")
-      .then((res) => {
-        this.setState({ friends: res.data });
-        this.setState({ SuccessMsg: res.statusText });
-      })
-      .catch((err) => console.log(err));
-  };
-  render() {
+//   componentDidMount() {
+//     this.getData();
+//   }
+//   getData = () => {
+//     axiosWithAuth()
+//       .get("/api/friends")
+//       .then((res) => {
+//         this.setState({ friends: res.data });
+//       })
+//       .catch((err) => console.log(err));
+//   };
+//   render() {
+//     return (
+//       <div>
+//         {this.state.friends.map((friend) => {
+//           return (
+//             <>
+//               {this.props.fetchingData && (
+//                 <div className="key spinner">
+//                   <Loader type="Puff" color="#204963" height="60" width="60" />
+//                   <p>Loading Data</p>
+//                 </div>
+//               )}
+
+//               <div>
+//                 <p>Friend's Name: {friend.name}</p>
+//                 <p>Friend's Age: {friend.age}</p>
+//                 <p>Friend's Email: {friend.email}</p>
+//               </div>
+//             </>
+//           );
+//         })}
+//       </div>
+//     );
+//   }
+// }
+
+// Changed class component into a function component!
+
+export default function FriendList(props) {
+    const [friends, setFriends] = useState([])
+    
+    useEffect(() => {
+       const getData = () => {
+        axiosWithAuth()
+          .get("/api/friends")
+          .then((res) => {
+              console.log('console here',res.data)
+            setFriends(res.data);
+          })
+          .catch((err) => console.log(err));
+       }
+       getData();
+    })
     return (
-      <div>
-        {this.state.friends.map((friend) => {
+        <div>
+        {friends.map((friend, key) => {
           return (
             <>
-              {this.props.fetchingData && (
+              {props.fetchingData && (
                 <div className="key spinner">
                   <Loader type="Puff" color="#204963" height="60" width="60" />
                   <p>Loading Data</p>
                 </div>
               )}
 
-              <div>
+              <div className='friends' key={key}>
                 <p>Friend's Name: {friend.name}</p>
                 <p>Friend's Age: {friend.age}</p>
                 <p>Friend's Email: {friend.email}</p>
@@ -42,7 +83,6 @@ class FriendList extends React.Component {
         })}
       </div>
     );
-  }
 }
 
-export default FriendList;
+

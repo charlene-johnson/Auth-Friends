3613c9fb76f23fed865860f274c71b254bd55ccd
charlene-johnson <charlene.j8234@gmail.com>
2020-07-10 23:07:10 -0400

    imported picture the correct way

--- friends/src/components/Welcome.js
+++ friends/src/components/Welcome.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { makeStyles } from '@material-ui/core/styles'
 import {Grid, Typography} from '@material-ui/core';
-import {Kitty} from '../img/kitteh.png'
+import Kitty from '../img/kitteh.png'
 
 const useStyles = makeStyles((theme)=> ({
     formGridItem: {
